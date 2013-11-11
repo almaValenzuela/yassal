f750e1d7b94484be92f42a9fb5fa910e2c5225f4
almaValenzuela <alma.delia.valenzuela.marquez@uabc.edu.mx>
2013-11-11 10:50:41 -0800

    archivos iniciales de ordenamiento

---
+++ UABC-programacionMatematica-algoritmos/src/edu/uabc/algoritmo/ordenamiento/setup/MainTest.java
@@ -0,0 +1,15 @@
+package edu.uabc.algoritmo.ordenamiento.setup;
+
+import edu.uabc.algoritmo.ordenamiento.utilerias.ListaInteger;
+import edu.uabc.algoritmo.ordenamiento.utilerias.Nodo;
+
+public class MainTest {
+	public static void main(String[] args) {
+		ListaInteger lista=new ListaInteger();
+		
+
+		
+		
+	}
+
+}
